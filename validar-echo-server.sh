868e1c561ea9d891cdba2df7c41c93960cd2e151
LucianaLarrosa <luciana.larrosa@gmail.com>
2026-03-16 14:22:33 -0300

    fix: use single = for string comparison in validar-echo-server.sh

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 test_msg="Hello server"
 response=$(docker run --rm --network tp0_testing_net alpine sh -c "echo '$test_msg' | nc server 12345")
-if [ "$response" == "$test_msg" ]; then
+if [ "$response" = "$test_msg" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
