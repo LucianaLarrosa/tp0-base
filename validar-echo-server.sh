73de3e9818f1aba5b6266917616ec25bd184b33d
LucianaLarrosa <luciana.larrosa@gmail.com>
2026-03-16 14:07:42 -0300

    feat: add validar-echo-server.sh for testing echo server functionality

---
+++ validar-echo-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+test_msg="Hello server"
+response=$(docker run --rm --network tp0_testing_net alpine sh -c "echo '$test_msg' | nc server 12345")
+if [ "$response" == "$test_msg" ]; then
+  echo "action: test_echo_server | result: success"
+else
+  echo "action: test_echo_server | result: fail"
+fi
