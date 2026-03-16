0ce890f77ca0bc2f4326ce990d5260da6a8cf62d
LucianaLarrosa <luciana.larrosa@gmail.com>
2026-03-15 22:26:50 -0300

    feat: add generar-compose.py and generar-compose.sh for Docker Compose generation

---
+++ generar-compose.py
@@ -0,0 +1,64 @@
+'''
+Este archivo se encarga de generar un archivo docker-compose-dev.yaml 
+a partir de los parámetros recibidos del archivo bash (nombre y cantidad de clientes).
+'''
+
+import sys
+import yaml
+
+def main():
+    file_name = sys.argv[1]
+    client_count = int(sys.argv[2])
+
+    data = {
+        "name": "tp0",
+        "services": {}
+    }
+
+    data["services"]["server"] = {
+        "container_name": "server",
+            "image": "server:latest",
+            "entrypoint": "python3 /main.py",
+            "environment": [
+                "PYTHONUNBUFFERED=1",
+                "LOGGING_LEVEL=DEBUG"
+            ],
+            "networks": [
+                "testing_net"
+            ]
+        }
+
+    for i in range(1, client_count+1):
+        data["services"][f"client{i}"] = {
+            "container_name": f"client{i}",
+            "image": "client:latest",
+            "entrypoint": "/client",
+            "environment": [
+                f"CLI_ID={i}",
+                "CLI_LOG_LEVEL=DEBUG"
+            ],
+            "networks": [
+                "testing_net"
+            ],
+            "depends_on": [
+                "server"
+            ]
+        }
+
+    data["networks"] = {
+        "testing_net": {
+            "ipam": {
+                "driver": "default",
+                "config": [
+                    {
+                        "subnet": "172.25.125.0/24"
+                    }
+                ]
+            }
+        }
+    }
+
+    with open(file_name, "w") as f:
+        yaml.dump(data, f, sort_keys=False)
+
+main()
