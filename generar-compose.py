e578462dbad9cf4876bb435aa93d809386ee7a8b
LucianaLarrosa <luciana.larrosa@gmail.com>
2026-03-16 23:03:07 -0300

    fix: correct formatting of environment variables in generar-compose.py

--- generar-compose.py
+++ generar-compose.py
@@ -36,11 +36,11 @@ def main():
             "image": "client:latest",
             "entrypoint": "/client",
             "environment": [
-                f"CLI_ID={i}"
-                "NOMBRE=Santiago Lionel"
-                "APELLIDO=Lorca"
-                "DOCUMENTO=30904465"
-                "NACIMIENTO=1999-03-17"
+                f"CLI_ID={i}",
+                "NOMBRE=Santiago Lionel",
+                "APELLIDO=Lorca",
+                "DOCUMENTO=30904465",
+                "NACIMIENTO=1999-03-17",
                 "NUMERO=7574"
             ],
             "networks": [
